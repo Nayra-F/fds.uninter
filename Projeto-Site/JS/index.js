19beab3f9a2ae33dd9ee7ed04546f650808292eb
Nayra-F <nayrafl20@gmail.com>
2025-07-06 23:41:16 -0300

    Delete Projeto-Site directory

--- Projeto-Site/JS/index.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById('meuBotao');
-const p = document.getElementById('minhaMensagem');
-
-botao.onclick = () => {
-  p.innerText = '"Cada passo pequeno é uma prova de que você não desistiu."';
-};
